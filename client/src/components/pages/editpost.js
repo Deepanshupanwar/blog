c6d8b30ae33e7b72b5c7ed12a32801a04e9a6d18
deepanshu <90817042+Deepanshupanwar@users.noreply.github.com>
2024-01-25 22:30:42 +0530

    Update editpost.js

--- client/src/components/pages/editpost.js
+++ client/src/components/pages/editpost.js
@@ -35,7 +35,7 @@ export default function EditPost(){
         if(file?.[0])
             data.set('file', file?.[0])
 
-        const response = await fetch('https://iblog-deepanshus-projects-b59175f2.vercel.app/api:4000/post',{
+        const response = await fetch('https://iblog-deepanshus-projects-b59175f2.vercel.app/api/post',{
             method: 'PUT',
             body: data,
             credentials: 'include'
