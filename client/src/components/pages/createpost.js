49baf032d39e0bc8e435a695c5db96e548d57e9f
deepanshu <90817042+Deepanshupanwar@users.noreply.github.com>
2024-01-25 22:20:05 +0530

    Update createpost.js

--- client/src/components/pages/createpost.js
+++ client/src/components/pages/createpost.js
@@ -24,7 +24,7 @@ export default function CreatePost(){
 
       ev.preventDefault();
 
-      const response= await fetch('https://iblog-deepanshus-projects-b59175f2.vercel.app/api:4000/post',
+      const response= await fetch('https://iblog-deepanshus-projects-b59175f2.vercel.app/api/post',
       {
         method: 'POST',
         body: data,
