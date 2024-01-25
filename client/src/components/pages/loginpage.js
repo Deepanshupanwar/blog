863d4c15d2b6eb6a2e0b7da482cc798495f4b4de
deepanshu <90817042+Deepanshupanwar@users.noreply.github.com>
2024-01-25 21:21:23 +0530

    Update loginpage.js

--- client/src/components/pages/loginpage.js
+++ client/src/components/pages/loginpage.js
@@ -10,7 +10,7 @@ export default function LoginPage(){
     const {setUserInfo} = useContext(userContext);
     async function login(ev){
         ev.preventDefault();
-        const response = await fetch('http://localhost:4000/login',{
+        const response = await fetch('https://iblog-deepanshus-projects-b59175f2.vercel.app/api/login',{
             method: 'POST',
             body: JSON.stringify({username, password}),
             headers: {'Content-Type':'application/json'},
