23a5a3fb2e9048903e69d4372c8d89b75f61bc6f
deepanshu <90817042+Deepanshupanwar@users.noreply.github.com>
2024-01-25 21:19:53 +0530

    Update registerpage.js

--- client/src/components/pages/registerpage.js
+++ client/src/components/pages/registerpage.js
@@ -9,7 +9,7 @@ export default function RegisterPage(){
     const {setUserInfo} = useContext(userContext);
     async function register(ev){
        ev.preventDefault(); 
-       const response= await fetch('http://localhost:4000/register',{
+       const response= await fetch('https://iblog-deepanshus-projects-b59175f2.vercel.app/api/register',{
         method:'POST',
         body: JSON.stringify({username,password}),
         headers:{'Content-type':'application/json'}
